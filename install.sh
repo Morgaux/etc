656c2b0d68b23d5946409661ddc27dca103f355d
Morgaux Meyer <mor@MultimediaPC.LabyrinthSolutions.local>
2019-01-22 13:27:35 +1300

    Started installer

---
+++ install.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+#
+# http://gitlab.com/morgaux/etc
+#
+# Installs etc files
+#
+##
+
+# Definitions
+SELFNAME = '~/etc Installer'
+SELFVER = "1.0.0"
+
+# Functions
+
+main()
+{
+	usage
+}
+
+usage()
+{
+	echo "$SELFNAME - v $SELFVER"
+	echo "usage:"
+	echo "./install.sh [OPTIONS]"
+	echo "options:"
+	echo "-h | --help	show this message"
+	exit 0
+}
+
+main
+
+exit $?
+
