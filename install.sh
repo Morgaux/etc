25c54dc541d39cb4c3e6a4f37f78f58448a77fcb
Morgaux Meyer <mor@MultimediaPC.LabyrinthSolutions.local>
2019-01-22 15:45:14 +1300

    Installer commit

--- install.sh
+++ install.sh
@@ -7,24 +7,54 @@
 ##
 
 # Definitions
-SELFNAME = '~/etc Installer'
-SELFVER = "1.0.0"
+NAME="~/etc Installer"
+VERSION="1.0.0"
+MESG="Installer commit"
 
 # Functions
 
 main()
 {
-	usage
+	readFlags $@
 }
 
 usage()
 {
-	echo "$SELFNAME - v $SELFVER"
+	echo "$NAME - v$VERSION"
 	echo "usage:"
-	echo "./install.sh [OPTIONS]"
+	echo "./install.sh [ OPTIONS ... ] DIRECTORY"
+	echo ""
 	echo "options:"
 	echo "-h | --help	show this message"
+	echo ""
 	exit 0
+}
+
+readFlags()
+{
+	while [ $# -gt 0 ] ; do
+		case $1 in
+			-h|--help)
+				usage
+				;;
+			-c|--commit)
+				selfCommit
+				;;
+		esac
+	done
+}
+
+selfCommit()
+{
+	[ -x "$(command -v git)" ] || die "git not installed"
+	git add .
+	git commit -m "$MESG"
+}
+
+die()
+{
+	echo "$NAME: FATAL: $@" 1>&2
+	exit 1
 }
 
 main $@
