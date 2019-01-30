604c37dc31c9b5a23baada589e9280abe0d78177
Morgaux Meyer <mor@MultimediaPC.LabyrinthSolutions.local>
2019-01-31 08:54:57 +1300

    installer v2

--- install.sh
+++ install.sh
@@ -18,21 +18,18 @@ MESG="Installer commit"
 
 main()
 {
+	[ -d "$HOME"/etc ] || mkdir "$HOME/etc"
 	[ $# -gt 0 ] && readFlags $@ || usage
 }
 
 usage()
 {
-	echo "usage:"
-	echo "./install.sh [ -h ] | [ -c | -p  MESSAGE ] | [ -i ]"
+	echo "$NAME v$VERSION - usage:"
+	echo "./install.sh [ -h ] | [ -i ]"
 	echo ""
 	echo "options:"
 	echo "-h, --help		show this message"
-	echo "-c message		commit local changes"
-	echo "-p message		push commits to repo"
-	echo "-i, --install DIRECTORY	install etc files"
-	echo ""
-	echo "$NAME - v$VERSION"
+	echo "-i, --install		install etc files"
 	exit 0
 }
 
@@ -42,20 +39,6 @@ readFlags()
 		case $1 in
 			-h|--help)
 				usage # exit implicit
-				;;
-			-c|--commit)
-				shift
-				[ $# -gt 0 ] || die "-c requires message"
-				MESG="$@"
-				selfCommit
-				exit $?
-				;;
-			-p|--push)
-				shift
-				[ $# -gt 0 ] || die "-p requires message"
-				MESG="$@"
-				selfPush
-				exit $?
 				;;
 			-i|--install)
 				shift
@@ -69,7 +52,6 @@ readFlags()
 
 installHome()
 {
-	mkdir -p $HOME/etc
 	echo "Installing profile..."
 	cp -uf $SRCDIR/profile ~/.profile
 	for i in kshrc vimrc ; do
@@ -87,26 +69,6 @@ installxenodm()
 	echo 'xset b off' >> /etc/X11/xenodm/Xsetup
 	echo 'xsetroot -solid black' >> /etc/X11/xenodm/Xsetup
 	sed -i 's/xconsole/#xconsole/' /etc/X11/xenodm/Xsetup_0
-}
-
-selfCommit()
-{
-	[ -x "$(command -v git)" ] || die "git not installed"
-	echo "Adding changes..."
-	git add .
-	echo "Commiting changes \"$MESG\""
-	git diff-index --quiet HEAD -- || git commit -m "$MESG"
-}
-
-selfPush()
-{
-	selfCommit
-	echo "Pushing commit..."
-	git log --oneline -1
-	#git push
-	until git push ; do
-		echo "Retrying" 1>&2
-	done
 }
 
 die()
